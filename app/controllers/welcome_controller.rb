d818a5f9e20248571de9ed7e5081a362ee27d483
ahmedreza80 <mdahmed@shiftind.com>
2019-05-13 10:15:27 +0530

    ghfgd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,71 @@
 class WelcomeController < ApplicationController
   def home
   end
+  def bt1
+  	@products = Product.all
+  end
+  def bt2
+  	@products = Product.all
+  end
+  def bt3
+  	@products = Product.all
+  end
+  def bt4
+  	@products = Product.all
+  end
+  def bt5
+  	@products = Product.all
+  end
+  def bt6
+  	@products = Product.all
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  def do1
+  	@products = Product.all
+  end
+  def do2
+  	@products = Product.all
+  end
+  def do3
+  	@products = Product.all
+  end
+  def do4
+  	@products = Product.all
+  end
+  def do5
+  	@products = Product.all
+  end
+  def do6
+  	@products = Product.all
+  end
+  def do7
+  	@products = Product.all
+  end
+  def do8
+  	@products = Product.all
+  end
+  def do9
+  	@products = Product.all
+  end
+  def do10
+  	@products = Product.all
+  end
+  def do11
+  	@products = Product.all
+  end
+
+
 
   def search
  	@products = Product.where('(name LIKE ? OR description LIKE ? )', "%#{params[:search]}%", "%#{params[:search]}%")
