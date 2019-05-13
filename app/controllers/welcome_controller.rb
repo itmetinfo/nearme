4af4a7ea72a3a1bb1e8c2a73b6d49aa60c664414
ahmedreza80 <mdahmed@shiftind.com>
2019-05-14 02:26:29 +0530

    dgfjdd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -19,18 +19,6 @@ class WelcomeController < ApplicationController
   def bt6
   	@products = Product.all
   end
-
-
-
-
-
-
-
-
-
-
-
-
   def do1
   	@products = Product.all
   end
@@ -64,37 +52,6 @@ class WelcomeController < ApplicationController
   def do11
     @products = Product.all
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
   def ea1
     @products = Product.all
@@ -152,9 +109,425 @@ class WelcomeController < ApplicationController
     @products = Product.all
   end
 
+def wed1
+    @products = Product.all
+  end
+  def wed2
+    @products = Product.all
+  end
+  def wed3
+    @products = Product.all
+  end
+  def wed4
+    @products = Product.all
+  end
+  def wed5
+    @products = Product.all
+  end
+  def wed6
+    @products = Product.all
+  end
+  def wed7
+    @products = Product.all
+  end
+  def wed8
+    @products = Product.all
+  end
+  def wed9
+    @products = Product.all
+  end
+  def wed10
+    @products = Product.all
+  end
+  def wed11
+    @products = Product.all
+  end
+  def wed12
+    @products = Product.all
+  end
+  def wed13
+    @products = Product.all
+  end
+  def wed14
+    @products = Product.all
+  end
+  def wed15
+    @products = Product.all
+  end
 
+def te1
+    @products = Product.all
+  end
+  def te2
+    @products = Product.all
+  end
+  def te3
+    @products = Product.all
+  end
+  def te4
+    @products = Product.all
+  end
+  def te5
+    @products = Product.all
+  end
+  def te6
+    @products = Product.all
+  end
+  def te7
+    @products = Product.all
+  end
+  def te8
+    @products = Product.all
+  end
 
+  def ren1
+    @products = Product.all
+  end
+  def ren2
+    @products = Product.all
+  end
+  def ren3
+    @products = Product.all
+  end
+  def ren4
+    @products = Product.all
+  end
+  def ren5
+    @products = Product.all
+  end
+  def ren6
+    @products = Product.all
+  end
 
+def gv1
+    @products = Product.all
+  end
+  def gv2
+    @products = Product.all
+  end
+  def gv3
+    @products = Product.all
+  end
+  def gv4
+    @products = Product.all
+  end
+  def gv5
+    @products = Product.all
+  end
+  def gv6
+    @products = Product.all
+  end
+  def gv7
+    @products = Product.all
+  end
+  def gv8
+    @products = Product.all
+  end
+  def gv9
+    @products = Product.all
+  end
+  def gv10
+    @products = Product.all
+  end
+  def gv11
+    @products = Product.all
+  end
+  def gv12
+    @products = Product.all
+  end
+  def gv13
+    @products = Product.all
+  end
+  def gv14
+    @products = Product.all
+  end
+  def gv15
+    @products = Product.all
+  end
+  def gv16
+    @products = Product.all
+  end
+  def gv17
+    @products = Product.all
+  end
+  def gv18
+    @products = Product.all
+  end
+  def gv19
+    @products = Product.all
+  end
+  def gv20
+    @products = Product.all
+  end
+  def gv21
+    @products = Product.all
+  end
+  def gv22
+    @products = Product.all
+  end
+  def gv23
+    @products = Product.all
+  end
+  def gv24
+    @products = Product.all
+  end
+  def gv25
+    @products = Product.all
+  end
+  def gv26
+    @products = Product.all
+  end
+  def gv27
+    @products = Product.all
+  end
+  def gv28
+    @products = Product.all
+  end
+  def gv29
+    @products = Product.all
+  end
+  def gv30
+    @products = Product.all
+  end
+  def gv31
+    @products = Product.all
+  end
+  def gv32
+    @products = Product.all
+  end
+  def gv33
+    @products = Product.all
+  end
+  def gv34
+    @products = Product.all
+  end
+  def gv35
+    @products = Product.all
+  end
+  def gv36
+    @products = Product.all
+  end
+  def gv37
+    @products = Product.all
+  end
+  def gv38
+    @products = Product.all
+  end
+  def gv39
+    @products = Product.all
+  end
+  def gv40
+    @products = Product.all
+  end
+  def gv41
+    @products = Product.all
+  end
+
+def fa1
+    @products = Product.all
+  end
+  def fa2
+    @products = Product.all
+  end
+  def fa3
+    @products = Product.all
+  end
+  def fa4
+    @products = Product.all
+  end
+  def fa5
+    @products = Product.all
+  end
+  def fa6
+    @products = Product.all
+  end
+  def fa7
+    @products = Product.all
+  end
+  def fa8
+    @products = Product.all
+  end
+  def fa9
+    @products = Product.all
+  end
+  def fa10
+    @products = Product.all
+  end
+  def fa11
+    @products = Product.all
+  end
+  def fa12
+    @products = Product.all
+  end
+  def fa13
+    @products = Product.all
+  end
+  def fa14
+    @products = Product.all
+  end
+  def fa15
+    @products = Product.all
+  end
+  def fa16
+    @products = Product.all
+  end
+  def fa17
+    @products = Product.all
+  end
+  def fa18
+    @products = Product.all
+  end
+  def fa19
+    @products = Product.all
+  end
+  def fa20
+    @products = Product.all
+  end
+  def fa21
+    @products = Product.all
+  end
+  def fa22
+    @products = Product.all
+  end
+  def fa23
+    @products = Product.all
+  end
+  def fa24
+    @products = Product.all
+  end
+  def fa25
+    @products = Product.all
+  end
+  def fa26
+    @products = Product.all
+  end
+  def fa27
+    @products = Product.all
+  end
+  def fa28
+    @products = Product.all
+  end
+  def fa29
+    @products = Product.all
+  end
+  def fa30
+    @products = Product.all
+  end
+  def fa31
+    @products = Product.all
+  end
+  def fa32
+    @products = Product.all
+  end
+  def fa33
+    @products = Product.all
+  end
+  def fa34
+    @products = Product.all
+  end
+  def fa35
+    @products = Product.all
+  end
+  def fa36
+    @products = Product.all
+  end
+  def fa37
+    @products = Product.all
+  end
+  def fa38
+    @products = Product.all
+  end
+  def fa39
+    @products = Product.all
+  end
+  def fa40
+    @products = Product.all
+  end
+  def fa41
+    @products = Product.all
+  end
+  def fa42
+    @products = Product.all
+  end
+  def fa43
+    @products = Product.all
+  end
+  def fa44
+    @products = Product.all
+  end
+  def fa45
+    @products = Product.all
+  end
+  def fa46
+    @products = Product.all
+  end
+  def fa47
+    @products = Product.all
+  end
+  def fa48
+    @products = Product.all
+  end
+  def fa49
+    @products = Product.all
+  end
+  def fa50
+    @products = Product.all
+  end
+  def fa51
+    @products = Product.all
+  end
+  def fa52
+    @products = Product.all
+  end
+  def fa53
+    @products = Product.all
+  end
+  def fa54
+    @products = Product.all
+  end
+
+def hard1
+    @products = Product.all
+  end
+  def hard2
+    @products = Product.all
+  end
+  def hard3
+    @products = Product.all
+  end
+  def hard4
+    @products = Product.all
+  end
+  def hard5
+    @products = Product.all
+  end
+  def hard6
+    @products = Product.all
+  end
+  def hard7
+    @products = Product.all
+  end
+  def hard8
+    @products = Product.all
+  end
+  def hard9
+    @products = Product.all
+  end
+  def hard10
+    @products = Product.all
+  end
+  def hard11
+    @products = Product.all
+  end
+  def hard12
+    @products = Product.all
+  end
+  def hard13
+    @products = Product.all
+  end
+  def hard14
+    @products = Product.all
+  end
 
   def search
  	@products = Product.where('(name LIKE ? OR description LIKE ? )', "%#{params[:search]}%", "%#{params[:search]}%")
